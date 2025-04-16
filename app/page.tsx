f4ab9eeaa1b8dbc41345f5619a35f2ba520cf2bc
Xianyx <pushkar44320@gmail.com>
2025-04-16 21:12:31 +0530

    dynamic content in react

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 
 import React from "react";
+import ProductInfo from "./ProductInfo";
+
+
 
 
 const page = () => {
 
-
+  const date = new Date();
 
   return (
 
-    <div>Next.js
+    <div>
+      <h1>Greet</h1>
+
+      <p>Date: {date.getDate()} </p>
+
+      <ProductInfo/>
 
 
     </div>
