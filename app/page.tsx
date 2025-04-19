9cfe6c33a06fc9fae2968992541ad4fdc7f2aed7
Xianyx <pushkar44320@gmail.com>
2025-04-19 22:33:13 +0530

    this is the start

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Counter from "./components/Counter";
-import UserProfile from "./components/UserProfile";
+
 
 const page = () => {
   return (
     <div>
-      <Counter />
-      <UserProfile />
+
     </div>
   );
 };
