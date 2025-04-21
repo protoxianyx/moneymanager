2b4e500c8127984fec8ac7ab9de7b1a63733ec30
Xianyx <pushkar44320@gmail.com>
2025-04-21 22:03:06 +0530

    form part 2

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,23 +1,32 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
+import React, { useRef, useState } from "react";
 
 interface FormData {
-    name: string,
-    email: string,
-    password: string
+  name: string;
+  email: string;
+  password: string;
 }
 
 const Form = () => {
+  const [submittedData, setSubmittedData] = useState<FormData>({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-    const [submittedData, setSubmittedData] = useState<FormData>({
-        name: '',
-        email: '',
-        password:''
-    })
+  const name = useRef<HTMLInputElement>(null);
+  const email = useRef<HTMLInputElement>(null);
+  const password = useRef<HTMLInputElement>(null);
+
   return (
-    <div>Form</div>
-  )
-}
+    <form>
+      <input type="text" placeholder="Enter your name" ref={name} className="m-1"/>
+      <input type="text" placeholder="Enter your name" ref={email} className="m-1"/>
+          <input type="text" placeholder="Enter your name" ref={password} className="m-1" />
+          <button type="submit"> Submit</button>
+    </form>
+  );
+};
 
-export default Form
+export default Form;
