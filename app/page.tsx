4ebd6d37b965f955f3a5a239294340ec8c284df4
Xianyx <pushkar44320@gmail.com>
2025-04-22 20:55:01 +0530

    chnages pv1

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import InsertRow from "./components/InsertRow";
+
 
 
 const page = () => {
   return (
     <div>
-      <InsertRow />
-      <p>Branch test 2</p>
+      
+      <p>Branch test 2 new</p>
     </div>
   );
 };
