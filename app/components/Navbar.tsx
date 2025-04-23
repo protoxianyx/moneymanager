39b794593b17876f83d3afd34d0ff4f93edfeeb3
Xianyx <pushkar44320@gmail.com>
2025-04-23 08:32:13 +0530

    usable navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,11 +4,8 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="flex flex-row mb-2 border-y-1 border-black p-2">
-      <Link
-        href={"/Todo"}
-        className="px-2 border-2 border-black rounded-xs mx-1"
-      >
-        Todo
+      <Link href={"/"} className="px-2 border-2 border-black rounded-xs mx-1">
+        Home
       </Link>
 
       <Link
