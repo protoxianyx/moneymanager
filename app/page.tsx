c121f88a3140178ead2e22b929bcf5a136d7103c
Xianyx <pushkar44320@gmail.com>
2025-04-23 07:48:36 +0530

    tidy

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-import InsertRow from "./components/InsertRow";
+
 
 
 const page = () => {
   return (
     <div>
-      <InsertRow />
-      <p>Branch test 2</p>
-      <p>Progress v2 start</p>
+      Home
     </div>
   );
 };
