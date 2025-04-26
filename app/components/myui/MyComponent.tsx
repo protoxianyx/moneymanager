08d1b2df50badccbf79a351458c16ec253e69104
Xianyx <pushkar44320@gmail.com>
2025-04-26 23:37:54 +0530

    use effect part 2

--- app/components/myui/MyComponent.tsx
+++ app/components/myui/MyComponent.tsx
@@ -1,22 +1,41 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
+type Product = {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+};
+
 const MyComponent = () => {
-  const [data, setData] = useState<null>(null);
+  const [data, setData] = useState<Product | null>(null);
 
   useEffect(() => {
     const fetchData = async () => {
-        try {
-          
-            const response = await fetch("https://dummyjson.com/product/1");
-            
+      try {
+        const response = await fetch("https://dummyjson.com/product/1");
+        const result = await response.json();
+        setData(result);
       } catch (error) {
         console.log("Error fetching data", error);
       }
     };
+
+    fetchData();
   }, []);
 
-  return <div>MyComponent</div>;
+  return (
+    <div>
+      {data ? (
+        <div>
+          <p>ID:</p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 };
 
 export default MyComponent;
