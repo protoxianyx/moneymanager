fbef1e191ec82334013e3f83be05bb5ded71c9f7
Xianyx <pushkar44320@gmail.com>
2025-05-29 01:46:09 +0530

    shadcn ui table

--- app/components/TLComponents/TodoList.tsx
+++ app/components/TLComponents/TodoList.tsx
@@ -1,36 +1,40 @@
 "use client";
 
 import React, { useState } from "react";
-import Holder from "./Holder";
-import { Table, TableCaption } from "../ui/table";
+// import Holder from "./Holder";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "../ui/table";
 
 interface Task {
-  id: string
-  task:string
+  id: string;
+  task: string;
 }
 
 const TodoList = () => {
-
   const [data, setData] = useState<Task[]>([]);
   const [input, setInput] = useState("");
-  const [numerator, setNumerator] = useState(1);
+  const [numerator, setNumerator] = useState(0);
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (input.trim() === '') return;
+    if (input.trim() === "") return;
 
     const addedTask: Task = {
-      id: Math.floor(Math.random() * 1e6).toString(), 
-      task: input.trim()
-    }
+      id: Math.floor(Math.random() * 1e6).toString(),
+      task: input.trim(),
+    };
 
-    setData(prev => [...prev, addedTask])
-    setInput('')
-    setNumerator(numerator+1)
-    
+    setData((prev) => [...prev, addedTask]);
+    setInput("");
+    setNumerator(numerator + 1);
   };
-
 
   return (
     <div>
@@ -42,23 +46,42 @@ const TodoList = () => {
               placeholder="Input Task"
               className="border-none bg-amber-300 m-2"
               value={input}
-              onChange={e => setInput(e.target.value)}
+              onChange={(e) => setInput(e.target.value)}
             />
             <button>Submit</button>
           </form>
         </div>
 
-        <div className="border-2 bg-amber-200 border-black px-2 py-1">
-          <ul>
-            {data.map((task) => (
-              <li key={task.id}> <Holder numerator={numerator} task={task.task} /> </li>
-            ))}
-          </ul>
-        </div>
-        <div>
-          <Table>
-            <TableCaption>Task Table</TableCaption>
-          </Table>
+        <div className="flex flex-row flex-nowrap">
+          {/* <div className="border-2 bg-amber-200 border-black px-2 py-1">
+            <ul>
+              {data.map((task) => (
+                <li key={task.id}>
+                  <Holder numerator={numerator} task={task.task} />
+                </li>
+              ))}
+            </ul>
+          </div> */}
+
+          <div>
+            <Table>
+              <TableCaption>Task Table</TableCaption>
+              <TableHead>
+                <TableRow>
+                  <TableHead>No.</TableHead>
+                  <TableHead className="text-center">Task</TableHead>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {data.map((task) => (
+                  <TableRow key={task.id}>
+                    <TableCell>{numerator}</TableCell>
+                    <TableCell>{task.task}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
         </div>
       </div>
     </div>
