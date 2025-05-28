9a6e41500134c5e4182997ab2ff957af690cba40
Xianyx <pushkar44320@gmail.com>
2025-05-29 03:20:29 +0530

    set up table

---
+++ app/components/MMComponents/MMLabel.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MMLabel = () => {
+  return (
+    <div>MMLabel</div>
+  )
+}
+
+export default MMLabel
--- app/moneymanager/page.tsx
+++ app/moneymanager/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+// import Chart1 from "@/components/MMComponents/Chart1";
+import MMInput from "@/components/MMComponents/MMInput";
+import MMLabel from "@/components/MMComponents/MMLabel";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import React from "react";
 
 const MoneyManager = () => {
   return (
-    <div>MoneyManager</div>
-  )
-}
+    <div>
+      <MMInput />
 
-export default MoneyManager
+      <Table>
+        <TableCaption>Money Manager Table</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Transaction Type</TableHead>
+            <TableHead className="w-[300px]">Spent/Added</TableHead>
+            <TableHead>Note</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          <TableRow>
+            <TableCell>
+              <MMLabel/>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default MoneyManager;
