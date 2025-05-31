114d5632249a899e722ab73c0bdd7bf26b6b3d8c
Xianyx <pushkar44320@gmail.com>
2025-06-01 01:39:15 +0530

    fiddling with components

---
+++ app/components/MMComponents/MMRadioGroup.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+export function MMRadioGroup() {
+  const [position, setPosition] = React.useState("bottom");
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">{position}</Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Transaction Type Tag</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+          <DropdownMenuRadioItem value="Spent">Spent</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="Added">Added</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
